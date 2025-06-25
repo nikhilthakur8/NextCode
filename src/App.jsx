4c7ee1e9cf5b15e34a0155ddf040d24272f56e16
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-06-25 20:02:27 +0530

    Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
+import { Analytics } from "@vercel/analytics/react";
 export const App = () => {
 	return (
 		<div>
 			<Outlet />
+			<Analytics />
 		</div>
 	);
 };
